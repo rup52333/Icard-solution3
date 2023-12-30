1bd6892f0d0db1709aec6776f84c09d285aa3fba
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2023-12-30 13:27:46 +0530

    4th modify

--- src/AdminLTELayout_school.js
+++ src/AdminLTELayout_school.js
@@ -186,7 +186,7 @@ const AdminLteSchoolForm = (props)  =>{
          // console.log(formData);
          setSchoolData(school);
          localStorage.setItem('school', JSON.stringify(school));
-          navigate(`/schoolSuccess`)
+          navigate(`/school/schoolSuccess`)
 }
 else{
    alert("Please fill in all required fields and upload images.");
